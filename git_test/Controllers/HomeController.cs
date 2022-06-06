5458e959407006a9bd11099f5f11331c38e31747
devmusty <mmustafayevm@gmail.com>
2022-06-06 19:55:56 +0400

    user implemented

--- git_test/Controllers/HomeController.cs
+++ git_test/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace git_test.Controllers
             return View();
         }
 
+        public IActionResult User()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
